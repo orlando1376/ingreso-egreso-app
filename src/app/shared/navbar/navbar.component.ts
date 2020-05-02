a699955a40fe822d27967da2aa2a45466086c6be
Orlando Franco Cespedes <orlandof@digitalware.com.co>
2020-05-02 17:24:39 -0500

    Seccion 9 - modificacion navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styles: []
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  userName: string;
+  userSubs: Subscription;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.userSubs = this.store.select('user')
+      .pipe( filter( ({user}) => user != null ) )
+      .subscribe( ({user}) => this.userName = user.nombre );
+  }
+
+  ngOnDestroy() {
+    this.userSubs.unsubscribe();
   }
 
 }
